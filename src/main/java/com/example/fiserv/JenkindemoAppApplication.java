2e10ef53a96093017f8d43aaedaa0c6a313a3af9
MY LAPTOP <MY LAPTOP@DESKTOP-VEBV59S.mshome.net>
2021-11-01 15:18:31 +0530

    master lakshita

---
+++ src/main/java/com/example/fiserv/JenkindemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkindemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkindemoAppApplication.class, args);
+	}
+
+}
